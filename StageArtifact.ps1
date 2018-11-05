055439e943a02b42158ea82dcb45078d72fdf7a7
gopalagarwal <gopal.agarwal@ad.loginradius.net>
2018-11-05 15:52:39 +0530

    new testing

--- StageArtifact.ps1
+++ StageArtifact.ps1
@@ -1,5 +1,6 @@
+mkdir 'C:\SA0'
 $target = "C:\inetpub\wwwroot\MusicWorld\" 
-
+mkdir 'C:\SA1'
 function DeleteIfExistsAndCreateEmptyFolder($dir )
 {
     if ( Test-Path $dir ) {    
@@ -10,10 +11,11 @@ function DeleteIfExistsAndCreateEmptyFolder($dir )
 }
 # Clean up target directory
 DeleteIfExistsAndCreateEmptyFolder($target )
-
+mkdir 'C:\SA2'
 # msdeploy creates a web artifact with multiple levels of folders. We only need the content 
 # of the folder that has Web.config within it 
  
 $path2 = "C:\temp\WebApp\MusicWorld\HelloGopal\*"
 
 Copy-Item $path2 $target -recurse -force
+mkdir 'C:\SA3'
