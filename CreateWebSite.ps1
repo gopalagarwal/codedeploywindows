a3f403d66b68fcf603bf2b7a3439bde1a2e7d593
gopalagarwal <gopal.agarwal@ad.loginradius.net>
2018-11-05 15:01:20 +0530

    final

--- CreateWebSite.ps1
+++ CreateWebSite.ps1
@@ -1,2 +1,2 @@
-New-WebSite -Name server1 -Port 1111 -PhysicalPath 'c:\\inetpub\\wwwroot\\MusicWorld' -Force
+New-WebSite -Name server1 -Port 1111 -PhysicalPath 'c:\inetpub\wwwroot\MusicWorld' -Force
 netsh advfirewall firewall add rule name='Open Port 1111' dir=in action=allow protocol=TCP localport=1111
