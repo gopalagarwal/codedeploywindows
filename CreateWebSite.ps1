6f471e5bcfeabcb5e8563d3d83b67222ba787fe0
gopalagarwal <gopal.agarwal@ad.loginradius.net>
2018-11-05 13:39:30 +0530

    thisis working

--- CreateWebSite.ps1
+++ CreateWebSite.ps1
@@ -1,1 +1,2 @@
-New-WebApplication -Site "Default Web Site" -Name MusicWorld -PhysicalPath c:\inetpub\wwwroot\MusicWorld -Force
+New-WebSite -Name server1 -Port 1111 -PhysicalPath 'c:\\inetpub\\wwwroot\\MusicWorld' -Force
+netsh advfirewall firewall add rule name='Open Port 1111' dir=in action=allow protocol=TCP localport=1111
--- StageArtifact.ps1
+++ StageArtifact.ps1
@@ -3,8 +3,7 @@ $target = "C:\inetpub\wwwroot\MusicWorld\"
 function DeleteIfExistsAndCreateEmptyFolder($dir )
 {
     if ( Test-Path $dir ) {    
-           Get-ChildItem -Path  $dir -Force -Recurse | Remove-Item -force –
-							  recurse
+           Get-ChildItem -Path  $dir -Force -Recurse | Remove-Item -force –recurse
            Remove-Item $dir -Force
     }
     New-Item -ItemType Directory -Force -Path $dir
@@ -31,6 +30,5 @@ function GetWebArtifactFolderPath($path)
     }
 }
 
-$path = GetWebArtifactFolderPath("C:\temp\WebApp\MusicWorld")
-$path2 = $path + "\*"
+$path2 = "C:\temp\WebApp\MusicWorld\*"
 Copy-Item $path2 $target -recurse -force
