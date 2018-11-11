44fae6f892c2201b9835ce8cd7aab079cb4caa0f
gopalagarwal <gopal.agarwal@ad.loginradius.net>
2018-11-11 16:04:21 +0530

    lol

--- CreateWebSite.ps1
+++ CreateWebSite.ps1
@@ -1,7 +1,7 @@
 Try
 {
-    New-WebSite -Name server1 -Port 1111 -PhysicalPath 'c:\inetpub\wwwroot\MusicWorld' -Force
-    netsh advfirewall firewall add rule name='Open Port 1111' dir=in action=allow protocol=TCP localport=1111
+    New-WebSite -Name server1 -Port 4444 -PhysicalPath 'c:\inetpub\wwwroot\MusicWorld' -Force
+    netsh advfirewall firewall add rule name='Open Port 4444' dir=in action=allow protocol=TCP localport=4444
 }
 Catch
 {
