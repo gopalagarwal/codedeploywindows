0301af071110c5ebf116d973924c8fde72280aed
gopalagarwal <gopal.agarwal@ad.loginradius.net>
2018-11-12 14:46:18 +0530

    finally working 1

--- StageArtifact.ps1
+++ StageArtifact.ps1
@@ -19,7 +19,7 @@ Try
     function DeleteIfExistsAndCreateEmptyFolder($dir )
     {
         if ( Test-Path $dir ) {    
-                Get-ChildItem -Path  $dir -Force -Recurse | Remove-Item -force –recurse
+                Get-ChildItem -Path  $dir -Force -Recurse | Remove-Item -force -recurse
                 Remove-Item $dir -Force
         }
         New-Item -ItemType Directory -Force -Path $dir
